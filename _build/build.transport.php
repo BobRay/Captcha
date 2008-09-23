24204fb172dda2edd5a099bb3189294aa52da9fd
splittingred <splittingred@24346d65-fb89-4855-a768-f5fb827ddd76>
2008-09-23 04:36:35 +0000

    [Captcha] - Added Captcha to the repository
    git-svn-id: http://svn.modxcms.com/svn/modx-components/captcha/trunk@143 24346d65-fb89-4855-a768-f5fb827ddd76

---
+++ _build/build.transport.php
@@ -0,0 +1,66 @@
+<?php
+$mtime = microtime();
+$mtime = explode(" ", $mtime);
+$mtime = $mtime[1] + $mtime[0];
+$tstart = $mtime;
+
+// get rid of time limit
+set_time_limit(0);
+
+// override with your own defines here (see build.config.sample.php)
+require_once dirname(__FILE__).'/build.config.php';
+
+require_once (MODX_CORE_PATH . 'model/modx/modx.class.php');
+$modx= new modX();
+$modx->initialize('mgr');
+$modx->setDebug(true);
+
+$name = 'captcha';
+$version = '1.0';
+$release = 'alpha';
+
+$modx->loadClass('transport.modPackageBuilder','',false, true);
+$builder = new modPackageBuilder($modx);
+$builder->create($name, $version, $release);
+$builder->registerNamespace($name,false,true);
+
+$sources= array (
+    'root' => dirname(dirname(__FILE__)) . '/',
+);
+
+// get the source from the actual snippet in your database OR
+// manually create the object, grabbing the source from a file
+$c= $modx->newObject('modPlugin');
+$c->set('name', 'captcha');
+$c->set('description', '<strong>1.0</strong> CAPTCHA Login Form');
+$c->set('category', 0);
+$c->set('snippet', file_get_contents($sources['root'] . 'plugin.captcha.php'));
+
+// create a transport vehicle for the data object
+$attributes= array(
+    XPDO_TRANSPORT_UNIQUE_KEY => 'name',
+    XPDO_TRANSPORT_UPDATE_OBJECT => true,
+);
+$vehicle = $builder->createVehicle($c, $attributes);
+$vehicle->resolve('file',array(
+    'source' => $sources['root'] . 'assets/plugins/captcha',
+    'target' => "return MODX_ASSETS_PATH . 'plugins/';",
+));
+$builder->putVehicle($vehicle);
+
+// load lexicon strings
+$builder->buildLexicon($sources['root'].'_build/lexicon/');
+
+// zip up the package
+$builder->pack();
+
+$mtime= microtime();
+$mtime= explode(" ", $mtime);
+$mtime= $mtime[1] + $mtime[0];
+$tend= $mtime;
+$totalTime= ($tend - $tstart);
+$totalTime= sprintf("%2.4f s", $totalTime);
+
+echo "\nExecution time: {$totalTime}\n";
+
+exit ();
---
+++ assets/plugins/captcha/plugin.captcha.php
@@ -0,0 +1,27 @@
+<?php
+$rt = "";
+
+if (isset ($modx->config['use_captcha']) && $modx->config['use_captcha'] == 1) {
+
+    $modx->lexicon->load('captcha:default');
+
+    if (isset ($modx->config['captcha_use_mathstring']) && $modx->config['use_mathstring'] == 1) {
+        $alt = $modx->lexicon("login_mathstring_message");
+    } else {
+        $alt = $modx->lexicon("login_captcha_message");
+    }
+
+    $captcha_image= '<a href="'.$_SERVER['PHP_SELF'].'" class="loginCaptcha"><img src="'.$modx->config['site_url'].'assets/plugins/captcha/captcha.php?rand='.rand().'" alt="'.$alt.'" /></a>';
+
+if (isset ($modx->config['captcha_use_mathstring']) && $modx->config['captcha_use_mathstring'] == 1) {
+        $captcha_prompt = '<p>'.$modx->lexicon("login_mathstring_message").'</p>';
+        $captcha_input= '<br /><br /><label>'.$modx->lexicon("captcha_mathstring_code").': <input type="text" name="captcha_code" tabindex="3" value="" /></label>';
+    } else {
+        $captcha_prompt = '<p>'.$modx->lexicon("login_captcha_message").'</p>';
+        $captcha_input= '<br /><br /><label>'.$modx->lexicon("captcha_code").': <input type="text" name="captcha_code" tabindex="3" value="" /></label>';
+    }
+
+    $rt = '<hr />'.$captcha_prompt.$captcha_image.$captcha_input;
+}
+
+$modx->event->_output = $rt;
