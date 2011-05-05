637181e58770446804a94d43ef02e6355ac00fcc
bobray <bobray@softville.com>
2011-05-04 23:00:44 -0500

    added Spanish language file

---
+++ core/components/captcha/lexicon/es/default.inc.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package captcha
+ * @subpackage lexicon
+ * @language es
+ */
+$_lang['area_captcha'] = 'CAPTCHA';
+$_lang['captcha_code'] = 'Codigo se seguridad';
+$_lang['captcha_mathstring_code'] = 'Respuesta';
+$_lang['login_captcha_error'] = 'Lo siento, el texto introducido no es correcto.  Por favor vuelva ha intentarlo.';
+$_lang['login_captcha_message'] = 'Por favor introduzca el codigo de seguridad indicado en la imagen. Si no puede leer el codigo, haga clik en la imagen para cambiarlo o contacte con su administrador del sitio web.';
+$_lang['login_mathstring_error'] = 'Lo siento, respuesta incorrecta. Por favor vuelve ha intentarlo.';
+$_lang['login_mathstring_message'] = 'Por favor resuelva la ecuación indicada en la imagen. Si no puede leer el codigo, haga clik en la imagen para cambiarlo o contacte con su administrador del sitio web.';
+
+/* lexicon strings for settings */
+$_lang['setting_captcha.enabled'] = 'Active CAPTCHA';
+$_lang['setting_captcha.enabled_desc'] = 'Si activar o no activar el soporte CAPTCHA .';
+$_lang['setting_captcha.height'] = 'La altura por defecto';
+$_lang['setting_captcha.height_desc'] = 'La altura por defecto de la imagen de CAPTCHA .';
+$_lang['setting_captcha.use_mathstring'] = 'Usa ecuación';
+$_lang['setting_captcha.use_mathstring_desc'] = 'Si activar o no activar la funcion de ecuación en captcha.';
+$_lang['setting_captcha.width'] = 'La anchura por defecto';
+$_lang['setting_captcha.width_desc'] = 'La anchura por defecto de la imagen de CAPTCHA.';
+$_lang['setting_captcha.words'] = 'Lista de palabras';
+$_lang['setting_captcha.words_desc'] = 'Las palabras por defecto que debería utilizar CAPTCHA.';
+?>
