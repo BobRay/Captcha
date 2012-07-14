4549a715f56b59ef3bb403a5fa981f8f0622b1ea
bobray <bobray@softville.com>
2012-07-13 21:41:04 -0500

    added german lexicon file

---
+++ core/components/captcha/lexicon/de/default.inc.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package captcha
+ * @subpackage lexicon
+ * @language de
+ * Translation by Igor <admin@igor-sweet.com>
+ */
+$_lang['area_captcha'] = 'CAPTCHA';
+$_lang['captcha_code'] = 'Sicherheitscode';
+$_lang['captcha_mathstring_code'] = 'Antwort';
+$_lang['login_captcha_error'] = 'Sie haben den angezeigen Text nicht Korrekt eingetragen. Bitte versuchen Sie es erneut.';
+$_lang['login_captcha_message'] = 'Bitte tragen Sie den angezeigten Sicherheitscode hier ein. Wenn sie den Code nicht lesen können, klicken Sie bitte auf das Bild um einen neuen zu Erzeugen.';
+$_lang['login_mathstring_error'] = 'Sie haben die Rechenaufgabe nicht Korrekt gelöst. Bitte versuchen Sie es erneut.';
+$_lang['login_mathstring_message'] = 'Bitte lösen Sie die Rechenaufgabe aus der Grafik. Wenn sie die Rechenaufgabe nicht lesen können, klicken Sie bitte auf das Bild um eine neue zu Erzeugen.';
+
+/* lexicon strings for settings */
+$_lang['setting_captcha.enabled'] = 'CAPTCHA aktivieren';
+$_lang['setting_captcha.enabled_desc'] = 'CAPTCHA support hinzufügen oder entfernen.';
+$_lang['setting_captcha.height'] = 'Standard Höhe';
+$_lang['setting_captcha.height_desc'] = 'Die standard Höhe des CAPTCHA Bilds.';
+$_lang['setting_captcha.use_mathstring'] = 'Rechenaufgabe stellen';
+$_lang['setting_captcha.use_mathstring_desc'] = 'Rechenaufgabe im Capatcha hinzufügen oder entfernen.';
+$_lang['setting_captcha.width'] = 'Standard Breite';
+$_lang['setting_captcha.width_desc'] = 'Die standard Breite des CAPTCHA Bilds.';
+$_lang['setting_captcha.words'] = 'Wort Liste';
+$_lang['setting_captcha.words_desc'] = 'Aufzählung der Standardwörter die im CAPTCHA Bild angezeigt werden sollen.';
