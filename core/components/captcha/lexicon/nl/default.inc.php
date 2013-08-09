98f71890dc428afc3a8cc464b9db459a8db34c37
bobray <bobray@softville.com>
2013-08-08 23:42:07 -0500

    Add NL lexicon file

---
+++ core/components/captcha/lexicon/nl/default.inc.php
@@ -0,0 +1,27 @@
+﻿<?php
+/**
+ *
+ * Dutch language file for Captcha extra
+ * Translation by Ivar Snel <ivar.snel@gmail.com>
+ * @package captcha
+ * @subpackage lexicon
+ */
+$_lang['area_captcha'] = 'CAPTCHA';
+$_lang['captcha_code'] = 'Veiligheidscode';
+$_lang['captcha_mathstring_code'] = 'Antwoord';
+$_lang['login_captcha_error'] = 'Sorry, u heeft niet correct de tekst ingevoerd zoals deze in het plaatje wordt getoond.  Probeert u het nog eens, a.u.b.';
+$_lang['login_captcha_message'] = 'Voert u a.u.b. de veiligheidscode in zoals deze in het plaatje wordt getoond. Als u deze niet kunt lezen, klik dan op het plaatje om een nieuwe te genereren of neem contact met uw administrator.';
+$_lang['login_mathstring_error'] = 'Sorry, verkeerd antwoord. Probeert u het nogmaals.';
+$_lang['login_mathstring_message'] = 'Lost u a.u.b. de rekensom op zoals deze in het plaatje wordt getoond. Als u deze niet kunt lezen, klik dan op het plaatje om een nieuwe te genereren of neem contact met uw administrator.';
+
+/* lexicon strings for settings */
+$_lang['setting_captcha.enabled'] = 'Inschakelen CAPTCHA';
+$_lang['setting_captcha.enabled_desc'] = 'Wel of geen CAPTCHA ondersteuning.';
+$_lang['setting_captcha.height'] = 'Standaard hoogte';
+$_lang['setting_captcha.height_desc'] = 'De standaard hoogte van het CAPTCHA venster.';
+$_lang['setting_captcha.use_mathstring'] = 'Gebruikt een rekensom';
+$_lang['setting_captcha.use_mathstring_desc'] = 'Wel of geen rekensom gebruiken in CAPTCHA.';
+$_lang['setting_captcha.width'] = 'Standaard breedte';
+$_lang['setting_captcha.width_desc'] = 'De standaard breedte van het CAPTCHA venster.';
+$_lang['setting_captcha.words'] = 'Woordlijst';
+$_lang['setting_captcha.words_desc'] = 'De standaard woordlijst welke CAPTCHA moet gebruiken.';
