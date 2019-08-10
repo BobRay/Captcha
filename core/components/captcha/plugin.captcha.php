7e6028fc33ffc52ebed127948a35495004835267
bobray <bobray@softville.com>
2019-08-10 13:38:21 -0500

    Fix critical bug in captcha plugin

--- core/components/captcha/plugin.captcha.php
+++ core/components/captcha/plugin.captcha.php
@@ -38,25 +38,19 @@
 /* @var $modx modX */
 switch ($modx->event->name) {
     case 'OnBeforeManagerLogin': /* register only for backend */
-        $version = $modx->getOption('settings_version');
-
-        $v = version_compare($version, '2.4.0-pl');
-        if  ($v < 0) {
-            $rt = array(true);
-        } else {
-            $rt = array(false);
-        }
+        $rt = true;
         $modx->lexicon->load('captcha:default');
-        if ($modx->getOption('captcha.enabled',null,false)) {
+        if ($modx->getOption('captcha.enabled', null, false)) {
             if (!isset ($_SESSION['veriword'])) {
-                 $rt = array('$_SESSION Variable not set');
-
-            }  else if ($_SESSION['veriword'] != $_POST['captcha_code']) {
-                /*$rt = "Debug: No Match: SESSION:".$_SESSION['veriword']." captcha_code:".$_POST['captcha_code']; */
-                if ($modx->getOption('captcha.use_mathstring',null,true)) {
-                    $rt= array($modx->lexicon('login_mathstring_error'));
-                } else {
-                    $rt = array($modx->lexicon('login_captcha_error'));
+                $rt = '$_SESSION Variable not set';
+            } else {
+                if ($_SESSION['veriword'] != $_POST['captcha_code']) {
+                    /*$rt = "Debug: No Match: SESSION:".$_SESSION['veriword']." captcha_code:".$_POST['captcha_code']; */
+                    if ($modx->getOption('captcha.use_mathstring', null, true)) {
+                        $rt = $modx->lexicon('login_mathstring_error');
+                    } else {
+                        $rt = $modx->lexicon('login_captcha_error');
+                    }
                 }
             }
         }
@@ -67,27 +61,29 @@ switch ($modx->event->name) {
     case 'OnManagerLoginFormRender': /* register only for backend */
         $rt = '';
 
-        if ($modx->getOption('captcha.enabled',null,false)) {
-
+        if ($modx->getOption('captcha.enabled', null, false)) {
             $modx->lexicon->load('captcha:default');
-
-            if ($modx->getOption('captcha.use_mathstring',null,true)) {
+            if ($modx->getOption('captcha.use_mathstring', null, true)) {
                 $alt = $modx->lexicon('login_mathstring_message');
             } else {
                 $alt = $modx->lexicon('login_captcha_message');
             }
 
-            $captcha_image= '<a href="'.$_SERVER['PHP_SELF'].'" class="loginCaptcha"><img src="'.$modx->getOption('site_url').'assets/components/captcha/captcha.php?rand='.rand().'" alt="'.$alt.'" /></a>';
+            $captcha_image = '<a href="' . $_SERVER['PHP_SELF'] . '" class="loginCaptcha"><img src="' .
+                $modx->getOption('site_url') . 'assets/components/captcha/captcha.php?rand=' . rand() .
+                '" alt="' . $alt . '" /></a>';
 
-            if ($modx->getOption('captcha_use_mathstring',null,true)) {
-                $captcha_prompt = '<p>'.$modx->lexicon("login_mathstring_message").'</p>';
-                $captcha_input= '<br /><br /><label>'.$modx->lexicon("captcha_mathstring_code").': <input type="text" name="captcha_code" value="" /></label>';
+            if ($modx->getOption('captcha_use_mathstring', null, true)) {
+                $captcha_prompt = '<p>' . $modx->lexicon("login_mathstring_message") . '</p>';
+                $captcha_input = '<br /><br /><label>' . $modx->lexicon("captcha_mathstring_code") .
+                    ': <input type="text" name="captcha_code" value="" /></label>';
             } else {
-                $captcha_prompt = '<p>'.$modx->lexicon("login_captcha_message").'</p>';
-                $captcha_input= '<br /><br /><label>'.$modx->lexicon("captcha_code").': <input type="text" name="captcha_code" value="" /></label>';
+                $captcha_prompt = '<p>' . $modx->lexicon("login_captcha_message") . '</p>';
+                $captcha_input = '<br /><br /><label>' . $modx->lexicon("captcha_code") .
+                    ': <input type="text" name="captcha_code" value="" /></label>';
             }
 
-            $rt = '<hr />'.$captcha_prompt.$captcha_image.$captcha_input;
+            $rt = '<hr />' . $captcha_prompt . $captcha_image . $captcha_input;
         }
 
         $modx->event->_output = $rt;
